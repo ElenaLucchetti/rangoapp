212af1ce8d93e3c2de93fbf67ec7f402d024d45b
Elena Lucchetti <2207893L@student.gla.ac.uk>
2017-02-10 13:03:27 +0000

    End of Chapter 8

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -8,14 +8,15 @@ class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default = 0)
     likes = models.IntegerField(default = 0)
+    slug = models.SlugField(blank=True)
     slug = models.SlugField(unique = True)
-
-    class Meta:
-        verbose_name_plural = 'Categories'
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -25,7 +26,6 @@ class Page(models.Model):
     title = models.CharField(max_length = 128)
     url = models.URLField()
     views = models.IntegerField(default = 0)
-   # fields = ["category", "name", "url" ]
 
     def __str__(self):
         return self.title
